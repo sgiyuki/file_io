c60c4d1ad29521282f0c658b3fd875db9eda47e1
YukiSugii <sugii.yuki8@gmail.com>
2025-02-11 15:17:44 +0900

    rename sample file name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import text_io
 
 if __name__ == "__main__":
     ### text_io sample
-    text = text_io.text_io("input\\text.txt")
+    text = text_io.text_io("input\sample.txt")
     text.open_file()
     # ['Hello World!!\n', 'This is text file!!\n', 'End of file!!\n']
     text.print_file()
